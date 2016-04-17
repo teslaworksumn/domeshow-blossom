d1fda3b3e8f34392331f1a2f408b4852dd167d5b
Ryan Fredlund <rfredlund13@gmail.com>
2016-04-16 23:44:50 -0500

    Example works. Unsure about others

--- blossom.ino
+++ blossom.ino
@@ -1,46 +1,102 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
+#include <Crc16.h>
 
-// the default address is 0x40
-Adafruit_PWMServoDriver* boards = new Adafruit_PWMServoDriver[9];
+Adafruit_PWMServoDriver *boards = new Adafruit_PWMServoDriver[9];
+Crc16 crc16;
+const int PIN_CTS = 3;
+const int PIN_RTS = 4;
 
 void setup()
 {
-//  Serial.begin(9600);
-//  Serial.println("16 channel PWM test!");
-
+  // Start serial
+  Serial.begin(9600);
+  Serial.println("Starting up");
+  
+  // Setup pins
+  pinMode(PIN_CTS, OUTPUT);
+  pinMode(PIN_RTS, INPUT);
+  
+  // Setup breakout boards
   for (uint8_t i = 0; i < 9; i++)
   {
-    Adafruit_PWMServoDriver* pwm = new Adafruit_PWMServoDriver(i);
+    // the default address is 0x40
+    Adafruit_PWMServoDriver *pwm = new Adafruit_PWMServoDriver(0x40 + i);
     pwm->begin();
-    pwm->setPWMFreq(1600);  // This is the maximum PWM frequency
+    pwm->setPWMFreq(1400);  // 1600? is the maximum PWM frequency
     boards[i] = *pwm;
   }
   
 }
 
-void loop()
+uint8_t *readData(uint16_t length)
 {
-  // Fill with random data
-  uint8_t* data = new uint8_t[144];
-  uint8_t* fill = new uint8_t[3];
-  fill[0] = 0;
-  fill[1] = 120;
-  fill[2] = 255;
-  for (int i = 0; i < 144; i++)
+  uint8_t *data = new uint8_t[length];
+  for (uint8_t i = 0; i < length; i++)
   {
-    data[i] = fill[i%3];
+    uint8_t datam = Serial.read();
+    data[i] = datam;
+    crc16.updateCrc(datam);
   }
-  
-  // Bit shift and send to boards
+  return data;
+}
+
+void sendData(uint8_t *data)
+{
   for (uint8_t board_idx = 0; board_idx < 9; board_idx++)
   {
     for (uint8_t channel = 0; channel < 16; channel++)
     {
-      boards[board_idx].setPWM(channel, 0, data[board_idx * 16 + channel] << 4);
+      int index = board_idx * 16 + channel;
+      boards[board_idx].setPWM(channel, 0, data[index] << 4);
     }
   }
+}
+
+boolean magicFound()
+{
+  if (Serial.peek() == 0xde) Serial.read();
+  else return false;
   
+  if (Serial.peek() == 0xad) Serial.read();
+  else return false;
+  
+  if (Serial.peek() == 0xbe) Serial.read();
+  else return false;
+  
+  if (Serial.peek() == 0xef) Serial.read();
+  else return false;
+
+  return true;
+}
+
+void loop()
+{
+  digitalWrite(PIN_CTS, HIGH);
+  if (Serial.available())
+  {
+    if (magicFound())
+    {
+      digitalWrite(PIN_CTS, LOW);
+      
+      uint8_t lengthLow = Serial.read();
+      uint16_t lengthHigh = Serial.read() << 8;
+      uint16_t length = lengthHigh + lengthLow;
+
+      crc16.clearCrc();
+      uint8_t *data = readData(length);
+
+      uint8_t crcLow = Serial.read();
+      uint16_t crcHigh = Serial.read() << 8;
+      uint16_t receivedCrc = crcHigh + crcLow;
+      if (crc16.getCrc() == receivedCrc)
+      {
+        sendData(data);
+        digitalWrite(PIN_CTS, HIGH);
+      }
+      
+    }
+  }
   
   
 }
